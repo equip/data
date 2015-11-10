2b2e6e006298ad81021f32032a940baa95e03a5e
elazar <tobias382@gmail.com>
2015-11-10 12:25:54 -0600

    Add DateAwareTrait->dateString()

--- tests/Traits/DateAwareTest.php
+++ tests/Traits/DateAwareTest.php
@@ -4,13 +4,23 @@ namespace SparkTests\Data\Traits;
 
 class DateAwareTest extends \PHPUnit_Framework_TestCase
 {
+    private $object;
+
+    protected function setUp()
+    {
+        $this->object = new DateAware;
+        $this->object->at = '2015-10-30 15:05:00';
+    }
+
     public function testDate()
     {
-        $object = new DateAware;
-        $object->at = '2015-10-30 15:05:00';
-
-        $date = $object->date('at');
-
+        $date = $this->object->date('at');
         $this->assertInstanceOf('Carbon\Carbon', $date);
+    }
+
+    public function testDateString()
+    {
+        $date = $this->object->dateString('at');
+        $this->assertSame('Fri, 30 Oct 2015 15:05:00 -0500', $date);
     }
 }
